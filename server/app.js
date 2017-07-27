b034131ac23ed5b77d2f899e6f1db0ad70d7f4a5
kokoh <kokohtan@gmail.com>
2017-07-27 20:15:14 +0700

    zomato api server

--- server/app.js
+++ server/app.js
@@ -4,16 +4,20 @@ const express = require('express')
 const app = express();
 const bodyParser = require('body-parser')
 
+const search = require('./router/search')
+const cors = require('cors')
+
 app.use(bodyParser.urlencoded({ extended:true }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
 
+app.use(cors());
+app.use('/', search);
 
 app.get('/', function(req,res){
   res.send('hai, project week 1 phase 2')
 })
-
 
 
 app.listen(3000)
