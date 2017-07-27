23c8bfa22279be2629d825b45daaee24113dfcab
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-07-27 17:42:45 +0700

    update fields fb login

--- server/controller/facebook.js
+++ server/controller/facebook.js
@@ -1,13 +1,22 @@
 'use strict'
 
 const FB  = require('fb')
-// const User = require('../models/User')
+const User = require('../models/users')
 
 
 function login(req,res){
   const accessToken = req.headers.token;
   FB.setAccessToken(accessToken)
-  FB.api('/me', function(response){
+  FB.api('/me', {fields: ['id','name','email', 'user_about_me']} ,function(response){
+    // User.create({
+    //   name: response
+    //   username: name
+    //   email:
+    //   fb_token: accessToken
+    // })
+    // .then(log=>{
+    //   res.send(response)
+    // })
     res.send(response)
   })
 }
