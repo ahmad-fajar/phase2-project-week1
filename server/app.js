83a2a96edfc22fab6ba7ba1f0549a83dbcbd583f
Sukmo Ronggo <hd.wijoyo@gmail.com>
2017-07-27 16:38:55 +0700

    start

---
+++ server/app.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended:true }));
+app.use(bodyParser.json());
+app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({type: 'application/x-www-form-urlencoded'}))
+
+
+app.get('/', function(req,res){
+  res.send('hai, project week 1 phase 1')
+})
+
+
+
+app.listen(3000)
